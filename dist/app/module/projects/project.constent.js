646d41658fc12d758478e407210b2d302cffd605
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2024-02-16 00:06:44 +0600

    add new

---
+++ dist/app/module/projects/project.constent.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.projectFilterableFields = exports.projectSearchableFields = void 0;
+exports.projectSearchableFields = ['searchTerm', 'projectName', 'projectType', 'tag'];
+exports.projectFilterableFields = [
+    'searchTerm',
+    'projectName',
+    'projectType',
+    'tag'
+];
